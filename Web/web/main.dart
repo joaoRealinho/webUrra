6a9d5d5b1a22194f17c2b555d9b8954b13243201
joaoRealinho <42327088+joaoRealinho@users.noreply.github.com>
2018-09-11 01:26:53 +0100

    Update main.dart

--- Web/web/main.dart
+++ Web/web/main.dart
@@ -6,12 +6,7 @@ import 'package:firebase/firebase.dart';
 void main() {
   //Inicia a conecção ao Firebase
   initializeApp(
-      apiKey: "AIzaSyAKq8Q9JZSvYRDu6p6yOysWEoEmXrB-CfI",
-      authDomain: "urra-e2ced.firebaseapp.com",
-      databaseURL: "https://urra-e2ced.firebaseio.com",
-      projectId: "urra-e2ced",
-      storageBucket: "urra-e2ced.appspot.com",
-      messagingSenderId: "290975447138");
+      
 
   bootstrap(AppComponent);
 }
